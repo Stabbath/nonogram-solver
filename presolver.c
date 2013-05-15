c45b19faf646853a1eec9878495eaaa8822d4958
Stabbath <francisco.pesquita@gmail.com>
2013-05-15 01:44:40 +0100

    Organised presolver.c comments

--- presolver.c
+++ presolver.c
@@ -8,13 +8,8 @@
   * presolve: O(L*N²)		Calls stackline on every row and on every column once to solve	 			*
   *							the easiest cells, to pave the way for the fuller linesolver.				*
   *																										*
-  * @param Puzzle* :																					*
+  * @param Puzzle* :		puzzle to preprocess														*
   *	@return int :			number of unsolved cells left in the puzzle									*
-  * 																									*
-  * @verbose : 				  Solves cells implicitly based on each line's native characteristics, ie	*
-  * 						block numbers and block sizes. Basically fetches all of the basic "easy"	*
-  *							information for a puzzle before the program is allowed to go into the more	*
-  *							complete algorithm.	For more information check the stackline function.		*
   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 int presolve(Puzzle* puzzle) {	//O(L*N²)
 	int unsolvedCellCount = puzzle->length[ROW] * puzzle->length[COL];
@@ -34,6 +29,9 @@ int presolve(Puzzle* puzzle) {	//O(L*N²)
 	return unsolvedCellCount;
 }
 
+
+
+#define IMPOSSIBLE	-1
 /** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   * stackline: O(N²)		Solves cells implicitly based on the line's native 							*
   *							characteristics, ie block number and block sizes.							*
@@ -43,42 +41,8 @@ int presolve(Puzzle* puzzle) {	//O(L*N²)
   * @param Line* :			line to solve																*
   * @param int :			length of this line 														*
   *	@return int :			number of solved cells, or -1 if an impossibility was discovered.			*
-  * 																									*
-  * @verbose : TODO																						*
   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#define IMPOSSIBLE	-1
 int stackline(Line* line, int length) {
- /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  * NOTE: With N*B, B is the average block size, meaning N*B is the sum of all block lengths*
-  *																							*
-  *	   F(x) is a function which returns y = hitrate*x, 0 <= hitrate <= 1 because only the	*
-  * blocks for which there is a superposition will lead to O(B-L+S). Worst case, ie every 	*
-  * block has a non-null superposition, means:  N*B = L-1  ,  F(N*(B-L+S)) = N*(B-L+S) =    *
-  *	=  L-1 - N*L + N*S																		*
-  *																							*
-  *     O(N)  +  max( (O(L), O(N*B), O(N*F(B-L+S)) )											*
-  * =   O(N + L + L-1 + L-1-N*L+N*S)				, N*B = L-1; -1 are neglibile			*
-  * =   O(N + 3L - N*L + N*S)						, S = N*B + N-1	= L-1 + N-1				*
-  * =   O(N + (3-N)*L + N*(L+N-2))															*
-  * =   O(N + (3-N)*L + N*L + N² - 2*N)														*
-  * =   O(N² + 3*L - N)																		*
-  * =   O(N² + 3*L)									, dN²/dL >> d(3*L)/dL					*
-  * =   O(N²)																				*
-  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-//alternative:
- /*																							*
-  *     O(N)  +  max( (O(L), O(N*B), O(N*F(B-L+S)) )										*
-  * =   O(N + L + N*B + L-1 - N*L + N*S)			, -1 is neglibile						*
-  * =   O((N+1)*B - (N-2)*L + N*S)					, S = N*B + N-1							*
-  * =   O((N+1)*B - (N-2)*L + N*(N*B+N-1))			, 										*
-  * =   O((N²+N+1)*B - (N-2)*L + N²-N 				, L = N*B + 1 							*
-  * =   O((N²+N+1)*B - (N-2)*(N*B+1) + N²-N													*
-  * =   O(2*N²*B - N*B + B - N + 2 + N² - N)												*
-  * =   O(N²*B - N*B + B + N² - N)															*
-  * =   O(N²*B + N²)																		*
-  * =   O(N²*(B+1))                                 , dN²/dL >> d(B+1)/dL                   *
-  * =   O(N²)           	 	                                               	            *
-  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 	int sum = getMinSumOfBlocksAndBlanks(line, 0);	//O(N) 
 	int cap = getLengthOfLargestBlock(line);	//O(N)
 
@@ -132,15 +96,9 @@ int stackline(Line* line, int length) {
 		return ret;
 	} else
 	if (length - sum < cap) {	//O(N*F(B-L+S))				//only situation in which stacking will do anything
-		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-		 *  Using simple math we can identify which cells must be filled and which remain unknown. The first *
-		 * length-sum cells are unaffected because length-sum is the difference between the first cell of the*
-		 * left-most and right-most positions of the any block in the line. Then, blocksize-(length-sum) is  *
-		 * the amount of cells which must be filled if ANY cells at all are to be filled (ie if there is a   *
-		 * superposition of the leftmost and rightmost possibilities). If no cells can be filled, we skip    *
-		 * blocksize cells to account for that block's placement, otherwise we skip another length-sum to the*
-		 * right of the block. Also, if length - sum < cap, we know there is no superposition for any block. *
-		 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+		 *  Uses simple math to can identify implied cells for general cases *
+		 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 		int n = 0;
 		i = length - sum;												//the first (length - sum) cells are unaffected, skip them
 		while (n < line->blockNum) {	//O(N*F(B-L+S))
